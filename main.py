355133f9ac72617f4d42056f046e82cf327aad0f
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2019-02-27 18:30:57 -0300

    added Panel, added .gitignore file

--- main.py
+++ main.py
@@ -7,15 +7,9 @@ class Frame(wx.Frame):
     def __init__(self, *args, **kw):
         super(Frame, self).__init__(*args, **kw)
 
+        self.Panel = Panel(self)
         self.CreateStatusBar()
         self.SetStatusText("This is an example program...")
-
-        self.textCtrl = wx.TextCtrl(self)
-        self.changeColorBtn = wx.Button(self, label="Change color")
-        self.resetColorBtn = wx.Button(self, label="Reset color")
-        self.changeColorBtn.Bind(event=wx.EVT_BUTTON, handler=partial(self.onChangeColor, target=self.textCtrl))
-        self.resetColorBtn.Bind(event=wx.EVT_BUTTON, handler=partial(self.onResetColor, target=self.textCtrl))
-        self._doLayout()
 
     def _makeMenuBar(self):
         fileMenu = wx.Menu()
@@ -33,6 +27,28 @@ class Frame(wx.Frame):
         self.Bind(wx.EVT_MENU, self.onExit,  exitItem)
         self.Bind(wx.EVT_MENU, self.onAbout, aboutItem)
 
+    def onExit(self, event):
+        self.Close(True)
+
+    def onAbout(self, event):
+        about = wx.adv.AboutDialogInfo()
+        about.Name = "Change wxTextCtrl colors example"
+        about.Version = "0.1"
+        about.Copyright = "(C) Emiliano Mesquita Drago"
+        about.Description = "An example for changing wxTextCtrl colors"
+        about.SetWebSite("https://github.com/mezka/wx-textctrlcolor-example.git")
+        wx.adv.AboutBox(about)
+
+class Panel(wx.Panel):
+    def __init__(self, *args, **kw):
+        super(Panel, self).__init__(*args, **kw)
+        self.textCtrl = wx.TextCtrl(self)
+        self.changeColorBtn = wx.Button(self, label="Change color")
+        self.resetColorBtn = wx.Button(self, label="Reset color")
+        self.changeColorBtn.Bind(event=wx.EVT_BUTTON, handler=partial(self.onChangeColor, target=self.textCtrl))
+        self.resetColorBtn.Bind(event=wx.EVT_BUTTON, handler=partial(self.onResetColor, target=self.textCtrl))
+        self._doLayout()
+    
     def _doLayout(self):
         hSizer = wx.BoxSizer(wx.HORIZONTAL)
         vSizer = wx.BoxSizer(wx.VERTICAL)
@@ -44,9 +60,6 @@ class Frame(wx.Frame):
 
         hSizer.SetMinSize(200, 100)
         self.SetSizerAndFit(hSizer)
-
-    def onExit(self, event):
-        self.Close(True)
     
     def onChangeColor(self, event, target):
         colors = [wx.RED, wx.BLUE, wx.GREEN]
@@ -58,14 +71,6 @@ class Frame(wx.Frame):
         target.BackgroundColour = wx.NullColour
         target.ForegroundColour = wx.NullColour
 
-    def onAbout(self, event):
-        about = wx.adv.AboutDialogInfo()
-        about.Name = "Change wxTextCtrl colors example"
-        about.Version = "0.1"
-        about.Copyright = "(C) Emiliano Mesquita Drago"
-        about.Description = "An example for changing wxTextCtrl colors"
-        about.SetWebSite("https://github.com/mezka/wx-textctrlcolor-example.git")
-        wx.adv.AboutBox(about)
 
 if __name__ == '__main__':
     app = wx.App()
